bd26a5ef52b60dcbdebad768f5b0b3f49ba67c08
Luke Zhan <luke.zhan@uwaterloo.ca>
2026-03-10 21:22:17 -0400

    Prepare scoped prerelease publish

--- test/node/cjs/index.js
+++ test/node/cjs/index.js
@@ -2,7 +2,7 @@ if ('Bun' in globalThis) {
   throw new Error('❌ Use Node.js to run this test!');
 }
 
-const { yoga } = require('@elysiajs/graphql-yoga');
+const { yoga } = require('@1spyral/elysia-graphql-yoga');
 
 if (typeof yoga !== 'function') {
   throw new Error('❌ CommonJS Node.js failed');
--- test/node/esm/index.js
+++ test/node/esm/index.js
@@ -2,7 +2,7 @@ if ('Bun' in globalThis) {
   throw new Error('❌ Use Node.js to run this test!');
 }
 
-import { yoga } from '@elysiajs/graphql-yoga';
+import { yoga } from '@1spyral/elysia-graphql-yoga';
 
 if (typeof yoga !== 'function') {
   throw new Error('❌ ESM Node.js failed');
